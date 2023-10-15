a80ad8a65bfab182f55d2acf96adcfb14408aac8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-15 21:23:11 +0300

    Add files via upload

--- Advanced.js
+++ Advanced.js
@@ -2,5 +2,14 @@
 // Interfaces
 const serverConfig = {
     protocol: 'https',
-    port: 3001
+    port: 3001,
+    role: 'admin',
+    log: (msg) => {
+        console.log(msg);
+    }
 };
+const startServer = (protocol, port, log) => {
+    log(`Server started on ${protocol}://server:${port}`);
+    return 'Server started';
+};
+startServer(serverConfig.protocol, serverConfig.port, serverConfig.log);
--- Advanced.ts
+++ Advanced.ts
@@ -3,20 +3,68 @@
 // Interfaces
 
 type Config = {protocol: 'http' | 'https', port: 3000 | 3001};
+type StartFunction = (protocol: 'http' | 'https', port: 3000 | 3001, log: Function) => string;
+
 
 interface IConfig {
     protocol: 'https',
     port: 3001
+    log: (msg: string) => void;
 }
 
 interface IRole {
     role: string;
 }
 
-
-
-const serverConfig: IConfig = {
-    protocol: 'https',
-    port: 3001
+interface IConfigWithIRole extends IConfig, IRole {
+    //test: string
 }
+
+const serverConfig: IConfigWithIRole = {
+    protocol: 'https',
+    port: 3001,
+    role: 'admin',
+    log: (msg: string): void => {
+        console.log(msg)
+    }
+}
+
+const startServer: StartFunction = (protocol: 'http' | 'https',
+                                    port: 3000 | 3001,
+                                    log: Function): 'Server started' => {
+
+    log(`Server started on ${protocol}://server:${port}`)
+
+    return 'Server started'
+};
+
+startServer(serverConfig.protocol, serverConfig.port, serverConfig.log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
