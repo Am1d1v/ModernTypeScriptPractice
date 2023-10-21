15e93ae38a5f5ca1ef08f33d4c024adeafc85514
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-21 16:03:18 +0300

    Add files via upload

--- Advanced.js
+++ Advanced.js
@@ -1,15 +1,28 @@
 "use strict";
 // Interfaces
+// function repairVehilce(vehicle: Vehicle): void{
+//     if(isCar(vehicle)){
+//         console.log(`Wheels: ${vehicle.wheels}`);
+//     } else if (isShip(vehicle)){
+//         console.log(`Sail: ${vehicle.sail}`);
+//     } else {
+//         //const something: never = vehicle; // never type
+//         console.log(vehicle.wings);
+//     }
+// }
 function repairVehilce(vehicle) {
-    if (isCar(vehicle)) {
-        console.log(`Wheels: ${vehicle.wheels}`);
-    }
-    else if (isShip(vehicle)) {
-        console.log(`Sail: ${vehicle.sail}`);
-    }
-    else {
-        //const something: never = vehicle; // never type
-        console.log(vehicle.wings);
+    switch (vehicle.name) {
+        case 'Car':
+            console.log(`Vehicle: Car, wheels: ${vehicle.wheels}`);
+            break;
+        case 'Ship':
+            console.log(`Vehicle: Ship, sail: ${vehicle.sail}`);
+            break;
+        case 'Airplane':
+            console.log(`Vehicle: Airplane, wings: ${vehicle.wings}`);
+            break;
+        default:
+            console.log(vehicle); // never type      
     }
 }
 function isCar(car) {
