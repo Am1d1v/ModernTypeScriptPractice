b82e323b4a9c4998b4604eaf90c156c5ad6a611b
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-22 23:12:58 +0300

    Add files via upload

---
+++ Generics.ts
@@ -0,0 +1,31 @@
+
+// Generics
+
+/*
+function processingData(data: string | number | boolean | string[]): string | number | boolean | string[]{
+
+    return data;
+}
+*/
+
+function processingData<T>(data: T): T{
+
+    return data;
+}
+const res1 = processingData(1); // 1
+const res2 = processingData([1, 2, 3]); // number[]
+const res3 = processingData('someName'); // 'someName'
+const res4 = processingData(['A', 'B', 'C']); // string[]
+
+
+
+
+
+
+
+
+
+
+
+
+
