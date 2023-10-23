69f87fdb6ccd786987a513f5e0d7cac4a375bad8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-23 13:07:43 +0300

    Add files via upload

--- Generics.js
+++ Generics.js
@@ -13,3 +13,10 @@ const res1 = processingData(1); // 1
 const res2 = processingData([1, 2, 3]); // number[]
 const res3 = processingData('someName'); // 'someName'
 const res4 = processingData(['A', 'B', 'C']); // string[]
+const res5 = processingData(100);
+const someProduct = {
+    design: 'ten'
+};
+const someProduct1 = {
+    design: 10
+};
--- Generics.ts
+++ Generics.ts
@@ -16,12 +16,30 @@ const res1 = processingData(1); // 1
 const res2 = processingData([1, 2, 3]); // number[]
 const res3 = processingData('someName'); // 'someName'
 const res4 = processingData(['A', 'B', 'C']); // string[]
+const res5 = processingData<number>(100);
+//const res6 = processingData<number>('100'); // error
 
 
+interface IPrintProductUK {
+    design: number;
 
+}
 
+interface IPrintProductES {
+    design: string;
+}
 
+interface IPrintProduct<T> {
+    design: T;
+}
 
+const someProduct: IPrintProduct<string> = {
+    design: 'ten'
+}
+
+const someProduct1: IPrintProduct<number> = {
+    design: 10
+}
 
 
 
